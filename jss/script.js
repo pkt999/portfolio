7c5e5a4e46d2023983b4bf9af38942ee4e918b91
Praveen Kumar <73178556+pkt999@users.noreply.github.com>
2025-02-02 14:39:35 +0530

    Update script.js

--- jss/script.js
+++ jss/script.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const modeToggle = document.getElementById("mode-toggle");
-    const navbar = document.getElementById("navbar"); // Target the navbar directly
 
     modeToggle.addEventListener("click", function() {
-        navbar.classList.toggle("dark-mode"); // Toggle dark mode for the navbar only
+        document.body.classList.toggle("dark-mode"); // Toggle dark mode on the body
     });
 });
